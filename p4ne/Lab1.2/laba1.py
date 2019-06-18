6b846fc3340830c547ba8ff827c0ced8f65a773d
jetsoft_katalev <as.katalev@jet.su>
2019-06-18 17:54:46 +0300

    done 1st lab

---
+++ p4ne/Lab1.2/laba1.py
@@ -0,0 +1,18 @@
+from matplotlib import pyplot
+from openpyxl import load_workbook
+
+wb = load_workbook('data_analysis_lab.xlsx')
+sheet = wb['Data']
+aСolumn = sheet['A'][1:]
+dСolumn = sheet['D'][1:]
+cСolumn = sheet['C'][1:]
+
+
+def getvalue(x): return x.value
+list1 = list(map(getvalue, aСolumn))
+list2 = list(map(getvalue, cСolumn))
+list3 = list(map(getvalue, dСolumn))
+
+pyplot.plot(list1, list3, label="Метка")
+pyplot.plot(list1, list2, label="Метка")
+pyplot.show()
